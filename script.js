7e34bb383901e888f94874b429fdf70e3b8d5f62
navjotmaan <navjotmaan137@gmail.com>
2025-09-09 14:49:02 +0000

    Added the Books object

---
+++ script.js
@@ -0,0 +1,9 @@
+function Books(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `${this.name} by ${this.author}, ${this.pages}, ${this.read}`;
+    }
+}
